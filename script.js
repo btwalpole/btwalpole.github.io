586f5cd7c5a0069d903435beb522bf932ac6e21e
Ben Walpole <benwalpole95@gmail.com>
2021-05-11 21:26:32 +0100

    nearly complete, WIP, committing changes

--- script.js
+++ script.js
@@ -124,3 +124,44 @@ leftButtons.forEach(button => {
         }
     })
 });
+
+//on hover show patterns
+
+let patterns = {
+    'afternoon-tea': 'images/patterns/p_afternoonTea.jpg',
+    'alaska' : 'images/patterns/p_alaska.jpg',
+    'autumn': 'images/patterns/p_autumn.jpg',
+    'ariel-aqua' : 'images/patterns/p_aquaAriel.jpg',
+    'ariel-red': 'images/patterns/p_arielRed.jpg',
+    'ariel-pastel': 'images/patterns/p_arielPastel.jpg',
+    'cherry-blossom-blush': 'images/patterns/p_cherryBlossomBlush.jpg',
+    'cherry-blossom-scarlet' : 'images/patterns/p_cherryBlossomScarlet.jpg',
+    'calypso' : 'images/patterns/p_calypso.jpg',
+    'daisy': 'images/patterns/p_daisy.jpg',
+    'dragon': 'images/patterns/p_dragon.jpg',
+    'feather': 'images/patterns/p_feather.jpg',
+    'golden-hour': 'images/patterns/p_goldenHour.jpg',
+    'jane': 'images/patterns/p_jane.jpg',
+    'seville' : 'images/patterns/p_seville.jpg',
+    'powerpuff' : 'images/patterns/p_powerpuff.jpg',
+    'monet': 'images/patterns/p_monet.jpg',
+    'rose': 'images/patterns/p_rose.jpg',
+    'wildflower': 'images/patterns/p_wildflower.jpg',
+}
+
+//for every image, add listener => find id, set src to correspoding pattern src
+
+let imgs = document.querySelectorAll('.photo-grid-item img');
+
+imgs.forEach( img => {
+    let initalImage = img.getAttribute('src');
+
+    img.addEventListener('mouseover', function() {
+        img.setAttribute('src', patterns[img.id]);
+    })
+
+    img.addEventListener('mouseout', function() {
+        img.setAttribute('src', initalImage);
+    })
+})
+
